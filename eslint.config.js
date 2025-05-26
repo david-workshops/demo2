1c1b02d3e131e4d42134c147305460644dce6871
David Jensenius <david@jensenius.com>
2025-05-26 10:33:54 -0700

    More bumping of all packages

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        // Node.js globals
+        global: "readonly",
+        process: "readonly",
+        Buffer: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        // Browser globals
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        // Jest globals
+        describe: "readonly",
+        it: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        jest: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      prettier: prettier,
+    },
+    rules: {
+      // TypeScript ESLint recommended rules
+      ...typescriptEslint.configs.recommended.rules,
+      // Prettier config to disable conflicting rules
+      ...prettierConfig.rules,
+      // Your custom rules
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
+  },
+];
